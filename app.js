c7c550639611e9ac1298f3bc76281df84bb28534
Niku <nokka200@gmail.com>
2024-10-05 18:06:33 +0300

    Lisätty CORs

--- app.js
+++ app.js
@@ -4,8 +4,35 @@ import { urheilijatRouter } from './controllers/urheilijat.js';
 const app = express();
 
 app.use(express.json());
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  res.setHeader("Content-type", "application/json");
+
+  // Pass to next layer of middleware
+  next();
+});
 
 // alustetaan routet
 app.use('/api/urheilijat', urheilijatRouter);
+
+
 
 export { app };
